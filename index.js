b41242682e6a0c4ce3e0a4de8a5082b8f4dc8629
Frank Cannon <cannonfl@gmail.com>
2018-03-21 22:19:39 -0700

    test

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const express = require("Express");
 const path = require("path");
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
-app.use(express.static("public"));
+app.set('port', process.env.PORT || 4000);
+//app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
   res.send(path.sesolve(__dirname, "public", "index.html"));
+});*/
+app.get('/', function (req, res) {
+  res.send('Hello Node + GitHub!');
 });
-
 let port = app.get('port');
+console.log(`port will be set to ${port}`);
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
+  console.log(`Server listening on port ${port}`);
 })
