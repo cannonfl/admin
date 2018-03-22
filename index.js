3a23b4dfa70cb089a1d7ec58b1743a88f95e0d77
Frank Cannon <cannonfl@gmail.com>
2018-03-21 21:19:20 -0700

    modify for heruko

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const express = require("Express");
 const path = require("path");
 const app = express();
 
-let port = process.env.PORT || 3000
-
+app.set('port', process.env.PORT || 3000);
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send(path.sesolve(__dirname, "public", "index.html"));
 });
 
+let port = app.get('port');
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
